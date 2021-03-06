0e1667b8490a585802a56db5d42b7570d5364bee
Kunduz <kuzanai@mail.ru>
2021-03-06 16:05:59 +0600

    Уф наконец то взошла в админ панель и создала комментарии

--- hello/webapp/forms.py
+++ hello/webapp/forms.py
@@ -3,5 +3,5 @@ from webapp.models import Comments
 
 class Commentform(forms.ModelForm):
     class Meta:
-        model=Comments
-        fields=('name', 'email', 'comment', 'status', 'created_at', 'updated_at')
+        model = Comments
+        fields = ('name', 'email', 'comment', 'status')
