e655be1e1c30731548de53b56dd62cee4aa859be
Kunduz <kuzanai@mail.ru>
2021-03-06 14:20:24 +0600

    Создала и регистрировала админ панель

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from webapp.models import Comments
 # Register your models here.
+
+
+class CommentAdmin(admin.ModelAdmin):
+    comment_display = ['id', 'name', 'email', 'comment']
+    list_filter = ['name', 'email']
+    search_fields = ['name', 'comment']
+    fields = ['id', 'name', 'email', 'comment', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+
+
+
+admin.site.register(Comments, CommentAdmin)
--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
+status_choice=[('active', 'активно'), ('blocked', 'заблокировано')]
+class Comments(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="name")
+    email = models.EmailField(max_length=100, null=True, blank=True, verbose_name='email')
+    comment = models.CharField(max_length=3000, null=True, blank=True, verbose_name='comment')
+    status = models.TextField(null=False, blank=False, choices=status_choice,  verbose_name='status', default='active')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created_at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated_at')
+
+    class Meta:
+        db_table ='comments'
+        verbose_name ='Комментарий'
+        verbose_name_plural ='Комментарии'
+
+
+    def __str__(self):
+        return f'{self.id}. {self.name}:{self.comment}'
+
+
